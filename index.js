82ff412b0ba29e83fadaa33f0603e473b1c03afc
Alexis Chavez <alexis.chavez17@outlook.com>
2023-03-16 13:57:56 -0700

    Complete JS Variables Lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+//let companyName = 'Scuber';
+//Defining company name as a constant
+const companyName = 'Scuber';
+// Define the most profitable neighborhood as a variable using the let keyword
+let mostProfitableNeighborhood = 'Chelsea';
+// Define the CEO of the company as a variable using the let keyword
+let companyCeo = 'Susan Smith';
+
